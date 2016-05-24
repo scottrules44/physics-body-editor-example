9a1366f3abed5d289eed5043ac1fdbb92ff7a0e1
scottrues44 <scottrules44@gmail.com>
2016-05-24 17:05:08 -0500

    1.1.1
    name change

--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
 local physics = require("physics")
 physics.start( )
 physics.setGravity( 0, 9.8 )
-local physicsBodyPlugin = require("physicsBodyPlugin")
+local physicsBodyEditor = require("physicsBodyEditor")
 local myObj = display.newImageRect( "example1.png", system.ResourceDirectory, 125, 125 )
 myObj.x, myObj.y = display.contentCenterX, display.contentCenterY
-physics.addBody( myObj, "static", physicsBodyPlugin.loadShape("example1", "test1",myObj))
+physics.addBody( myObj, "static", physicsBodyEditor.loadShape("example1", "test1",myObj))
 
 local myBox = display.newRect( display.contentCenterX-30, display.contentCenterY-100, 30, 30 )
 physics.addBody( myBox, "dynamic")
