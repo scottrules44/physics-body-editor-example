b11de44e0df4ace278fd5675d5ae46b4937844de
scottrues44 <scottrules44@gmail.com>
2016-05-24 19:06:02 -0500

    1.1.3
    plugin.plugin.physicsBodyEditor
    
    fixed in main .lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local physics = require("physics")
 physics.start( )
 physics.setGravity( 0, 9.8 )
-local physicsBodyEditor = require("physicsBodyEditor")
+local physicsBodyEditor = require("plugin.physicsBodyEditor")
 local myObj = display.newImageRect( "example1.png", system.ResourceDirectory, 125, 125 )
 myObj.x, myObj.y = display.contentCenterX, display.contentCenterY
 physics.addBody( myObj, "static", physicsBodyEditor.loadShape("example1", "test1",myObj))
